69b704bfee5f503186e99fe04e6ef779fa13f647
zstardustttt <93951756+zSt4rdust@users.noreply.github.com>
2026-01-16 04:29:40 +0700

    changes...

--- Assets/_Project/Scripts/Items/HuananV2.cs
+++ Assets/_Project/Scripts/Items/HuananV2.cs
@@ -1,7 +1,9 @@
 using Game.Core.Items;
+using Game.Core.Maps;
 using Game.Core.Projectiles;
 using Game.Player;
 using Game.Projectiles;
+using Mirror;
 using UnityEngine;
 
 namespace Game.Items
@@ -12,6 +14,17 @@ namespace Game.Items
 
         public override void Use(PlayerBase user, ItemUseClientContext context)
         {
+            if (Physics.Linecast(context.headPosition, context.visualPosition, out var hit, LayerMask.GetMask("Enviroment")))
+            {
+                var explosion = Instantiate(projectile.explosionPrefab.gameObject, hit.point, Quaternion.identity, new InstantiateParameters()
+                {
+                    scene = MapLoader.loadedMap.scene
+                });
+                explosion.GetComponent<DamageDealer>().owner = user;
+                NetworkServer.Spawn(explosion);
+                return;
+            }
+
             var finalRotation = context.crosshairHit ? Quaternion.LookRotation(context.crosshairHitPoint - context.visualPosition) : context.visualRotation;
             var proj = PredictableProjectile.Spawn(projectile, user, context.visualPosition, finalRotation, context.useTime);
             proj.Init();
