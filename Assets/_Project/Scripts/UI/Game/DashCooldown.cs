61761651bf0432e33efe90872c441a3c7eec06d8
zstardustttt <93951756+zSt4rdust@users.noreply.github.com>
2026-01-19 02:33:22 +0700

    overhaul dash cd

--- Assets/_Project/Scripts/UI/Game/DashCooldown.cs
+++ Assets/_Project/Scripts/UI/Game/DashCooldown.cs
@@ -7,37 +7,53 @@ namespace Game.UI.Game
 {
     public class DashCooldown : MonoBehaviour
     {
-        public Slider dashCooldownSlider;
-        private bool _cooldown;
+        public Slider[] dashCooldownSliders;
+        private bool _cooldownActive;
+        private float _cooldown;
 
         private void Awake()
         {
             EventBus<OnDash>.Listen((data) =>
             {
-                dashCooldownSlider.maxValue = data.cooldown;
-                dashCooldownSlider.value = data.cooldown;
+                foreach (var slider in dashCooldownSliders)
+                {
+                    slider.maxValue = data.cooldown;
+                    slider.value = data.cooldown;
+                }
 
-                _cooldown = false;
+                _cooldown = data.cooldown;
+                _cooldownActive = false;
             });
 
             EventBus<OnEndDash>.Listen((data) =>
             {
                 if (data.reset)
                 {
-                    dashCooldownSlider.value = 0f;
+                    foreach (var slider in dashCooldownSliders)
+                    {
+                        slider.value = 0f;
+                    }
+
+                    _cooldown = 0f;
                     return;
                 }
 
-                _cooldown = true;
+                _cooldownActive = true;
             });
         }
 
         private void Update()
         {
-            if (!_cooldown) return;
+            if (!_cooldownActive) return;
 
-            dashCooldownSlider.value -= Time.deltaTime;
-            if (dashCooldownSlider.value <= 0f) _cooldown = false;
+            _cooldown -= Time.deltaTime;
+            _cooldown = Mathf.Max(0f, _cooldown);
+            if (_cooldown <= 0f) _cooldownActive = false;
+
+            foreach (var slider in dashCooldownSliders)
+            {
+                slider.value = _cooldown;
+            }
         }
     }
 }
